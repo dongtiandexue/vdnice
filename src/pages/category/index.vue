11c320e3e71879fde4eeae1fcaf5b6709ebe21f8
huawei_code1994 <huawei_code@163.com>
2023-04-29 22:05:59 +0800

    微信小程序项目底部导航开发

---
+++ src/pages/category/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>分类</div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, toRefs, watch, onMounted, getCurrentInstance } from "vue";
+
+const emit = defineEmits([""]);
+const props = defineProps({
+  propData: {
+    type: String,
+    default: "",
+  },
+});
+//const { } = toRefs(props);
+
+const { data } = toRefs(
+  reactive({
+    //定义数组和对象
+    data: "",
+  })
+);
+</script>
+<style lang="scss" scoped></style>
