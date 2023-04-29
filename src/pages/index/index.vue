7eb0dce7f98b95fdec38c9dabdcce91a4fcf7b77
huawei_code1994 <huawei_code@163.com>
2023-04-29 10:47:39 +0800

    unocss 配置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,14 +2,14 @@
   <view class="content">
     <image class="logo" src="/static/logo.png" />
     <view class="text-area">
-      <text class="title">{{ title }}</text>
+      <text color-red text-50>{{ title }}</text>
     </view>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+import { ref } from "vue";
+const title = ref("Hello");
 </script>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
+import Unocss from 'unocss/vite'
 import uni from "@dcloudio/vite-plugin-uni";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [uni(), Unocss()],
 });
